c62a3fcc58069bbbff16c53ab29fd00ff47a2851
Chirag Verma <cv091998@gmail.com>
2020-03-11 20:49:39 +0530

    Update tensorflow js.py

--- Tensorflow.js/tensorflow js.py
+++ Tensorflow.js/tensorflow js.py
@@ -1,4 +1,5 @@
 # To convert your keras model into tensorflow.js model which is suitable to use with javascript.
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -9,5 +10,4 @@ import tensorflowjs as tfjs
 bring_model = tk.keras.models.load_model("... Your Model ...")
 
 tfjs.converters.save_keras_model(bring_model, "tensorflow.js model")
-
 
